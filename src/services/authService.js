b17d71f93ab29f53eb2dfbf2ba8dd0e4d76ed32c
axoblade <axoblade@gmail.com>
2025-03-02 10:53:26 +0300

    Add function to generate random password

--- src/services/authService.js
+++ src/services/authService.js
@@ -354,6 +354,17 @@ const addUser = async (email, name, phone, roleId, verificationLinkUrl) => {
 	return user;
 };
 
+function generateRandomPassword(length = 12) {
+	const charset =
+		"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%&*()=+";
+	let password = "";
+	for (let i = 0; i < length; i++) {
+		const randomIndex = Math.floor(Math.random() * charset.length);
+		password += charset[randomIndex];
+	}
+	return password;
+}
+
 module.exports = {
 	register,
 	login,
