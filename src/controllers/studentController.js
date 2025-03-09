d5fc6a2544bcece1ba4b1c0b00389286aea97874
axoblade <axoblade@gmail.com>
2025-03-09 11:43:26 +0300

    log user

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -2,6 +2,7 @@ const prisma = require("prisma");
 
 const createStudent = async (req, res) => {
 	try {
+		console.log(req.user);
 		res.success("SUCCESS");
 	} catch (error) {
 		res.error("400", "Internal Server Error");
